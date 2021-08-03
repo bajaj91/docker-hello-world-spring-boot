ce07820262b43838753c556ee282903a65881c9b
amit bajaj <amit.bajaj@pl.ibm.com>
2021-08-03 11:42:59 +0200

    ingress route added

--- angular-ui/src/app/app.component.ts
+++ angular-ui/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   response = "No data loaded, yet";
   constructor(private http: HttpClient) 
   { 
-    this.http.get('http://20.62.149.198:8080', {responseType: 'text'}).subscribe((response: any) => {
+    this.http.get('http://spring-demo-api-app.aks.cloudapp.azure.com', {responseType: 'text'}).subscribe((response: any) => {
       console.log(response);
 	  this.response = response;		
 	});
